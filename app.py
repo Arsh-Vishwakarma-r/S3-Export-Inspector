22633c94afd7db7a52fefe9b29ac3c0127da4153
Arsh-Vishwakarma-r <Arsh.Vishwakarma@rishabhsoft.com>
2025-09-25 10:13:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -384,7 +384,7 @@ if st.session_state.show_results and s3_path_input:
                     #bean {{
                         position: absolute;
                         top: -70px;
-                        left: 1550px;
+                        left: 1250px;
                         width: 80px;
                         height: 150px;
                         background-image: url('data:image/png;base64,{bean_base64}');
@@ -687,6 +687,7 @@ if st.session_state.show_results and s3_path_input:
                 data = df_result["Is New Frame?"].value_counts()
                 fig3 = make_pie_chart(data.index, data.values, ["#ff9800", "#009688"])
                 st.plotly_chart(fig3, use_container_width=True)
+
 
 
 
